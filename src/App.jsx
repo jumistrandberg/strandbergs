10c63149d7d16b07284618a1f6265fbbd925f80c
Jumi <feliciastrandberg@gmail.com>
2026-01-21 17:14:40 +0100

    Imports components and mounts

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import './App.css'
-import HeroComponent from './components/Hero/HeroComponent'
+import HeroComponent from './components/HeroComponent'
+import AboutSection from './components/AboutSection'
+import TeamSection from './components/TeamSection'
+import ContactSection from './components/ContactSection'
 
 function App() {
 
   return (
     <>
      <HeroComponent />
+     <AboutSection />
+     <TeamSection />  
+     <ContactSection />
     </>
   )
 }
