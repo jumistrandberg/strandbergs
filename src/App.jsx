8f57bbcd256b08eaf4321ffc2fb47e959f0d7c43
Jumi <feliciastrandberg@gmail.com>
2026-07-13 19:43:44 +0200

    Implements and applies routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,38 @@
-import './App.css'
-import HeroComponent from './components/HeroComponent'
-import AboutSection from './components/AboutSection'
-import TeamSection from './components/TeamSection'
-import ContactSection from './components/ContactSection'
-import BlobComponent from './components/BlobComponent'
+import "./App.css";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Home from "./views/Home";
+import About from "./views/About";
+import Services from "./views/Services";
+import Career from "./views/Career";
+import Contact from "./views/Contact";
 
 function App() {
-
   return (
-    <>
-     <BlobComponent />
-     <HeroComponent />
-     <AboutSection />
-     <TeamSection />  
-     <ContactSection />
-    </>
-  )
+      <BrowserRouter>
+        {/* Navigation */}
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+          <Link to="/services">Services</Link>
+          <Link to="/career">Career</Link>
+          <Link to="/contact">Contact</Link>
+        </nav>
+
+        {/* Routes  */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/career" element={<Career />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+        {/* <BlobComponent />
+        <HeroComponent />
+        <AboutSection />
+        <TeamSection />
+        <ContactSection /> */}
+      </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
---
+++ src/views/Home.jsx
@@ -0,0 +1,13 @@
+import HeroComponent from "../components/HeroComponent";
+import BlobComponent from "../components/BlobComponent";
+
+const Home = () => {
+  return (
+    <>
+      <BlobComponent />
+      <HeroComponent />
+    </>
+  );
+};
+
+export default Home;
