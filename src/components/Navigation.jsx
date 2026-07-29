d4a13cb644eb63a02353c09a8e34c983cb316671
Jumi <feliciastrandberg@gmail.com>
2026-07-29 07:45:07 +0200

    Fixes wrongful capitalisation on import

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import "../styles/Navigation.css";
+import "../styles/navigation.css";
 
 const links = [
   { to: "#home", label: "Hem" },
