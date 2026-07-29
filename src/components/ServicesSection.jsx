b75f6e0dbd484b217d530243d21f7f350e0d189d
Jumi <feliciastrandberg@gmail.com>
2026-07-29 21:57:43 +0200

    Finishes styling for services

--- src/components/ServicesSection.jsx
+++ src/components/ServicesSection.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import "../styles/services.css";
+import "../App.css";
 
 const ServicesSection = () => {
   return (
-    <div>ServicesSection</div>
-  )
-}
+    <div id="services-section">
+      <div classname="services-intro-container">
+        <h2>Tjänster</h2>
+        <p className="subtitle">
+          Vi erbjuder skräddarsydd personlig assistans för allas rätt till ett
+          värdigt liv
+        </p>
+      </div>
 
-export default ServicesSection
+      <div className="grid-container">
+        <div className="grid-box">
+          <h5>Personlig assistans</h5>
+          <p>Personlig assistans</p>
+        </div>
+        <div className="grid-box">
+          <h5>Rådgivning</h5>
+          <p>Personlig assistans</p>
+        </div>
+        <div className="grid-box">
+          <h5>herj</h5>
+          <p>Personlig assistans</p>
+        </div>
+        <div className="grid-box">
+          <h5>herj</h5>
+          <p>Personlig assistans</p>
+        </div>
+      </div>
+
+    <div className="description-box"> 
+
+    </div>
+        <p style={{textAlign: "left"}}>
+        Vi tar ett helhetsansvar för Din assistans. Du har alltid full inblick i
+        vad som görs. Du styr själv hur mycket eller lite stöd du vill ha
+        i din assistans. Vi arbetar efter FKs riktlinjer och följer de lagar
+        och avtal som reglerar LSS och LASS.
+      </p>
+    </div>
+  );
+};
+
+export default ServicesSection;
