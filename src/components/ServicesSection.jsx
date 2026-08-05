bd2b34fd658c66c921aa710d6756c96d11812fb3
Jumi <feliciastrandberg@gmail.com>
2026-08-05 17:55:56 +0200

    Adds base for GitHub Pages

--- src/components/ServicesSection.jsx
+++ src/components/ServicesSection.jsx
@@ -12,33 +12,39 @@ const ServicesSection = () => {
         </p>
       </div>
 
-      <div className="grid-container">
-        <div className="grid-box">
-          <h5>Personlig assistans</h5>
-          <p>Personlig assistans</p>
-        </div>
-        <div className="grid-box">
-          <h5>Rådgivning</h5>
-          <p>Personlig assistans</p>
-        </div>
-        <div className="grid-box">
-          <h5>herj</h5>
-          <p>Personlig assistans</p>
-        </div>
-        <div className="grid-box">
-          <h5>herj</h5>
-          <p>Personlig assistans</p>
-        </div>
+      <div>
+        <h5>Personlig assistans</h5>
+        <p>
+          Vi tar det fulla arbetsgivaransvaret för dina assistenter och ser till
+          att vi rekryterar rätt personer som får trygg handledning och bra lön
+          så att de vill och kan stanna med våra brukare.
+        </p>
+      </div>
+      <div className="grid-box">
+        <h5>Rådgivning</h5>
+        <p>Personlig assistans</p>
+      </div>
+      <div className="grid-box">
+        <h5>herj</h5>
+        <p>Personlig assistans</p>
+      </div>
+      <div className="grid-box">
+        <h5>herj</h5>
+        <p>Personlig assistans</p>
       </div>
 
-    <div className="description-box"> 
+      {/* <div className="grid-container">
+        <div className="grid-box">
 
-    </div>
-        <p style={{textAlign: "left"}}>
+        </div>
+      </div> */}
+
+      <div className="description-box"></div>
+      <p style={{ textAlign: "left" }}>
         Vi tar ett helhetsansvar för Din assistans. Du har alltid full inblick i
-        vad som görs. Du styr själv hur mycket eller lite stöd du vill ha
-        i din assistans. Vi arbetar efter FKs riktlinjer och följer de lagar
-        och avtal som reglerar LSS och LASS.
+        vad som görs. Du styr själv hur mycket eller lite stöd du vill ha i din
+        assistans. Vi arbetar efter FKs riktlinjer och följer de lagar och avtal
+        som reglerar LSS och LASS.
       </p>
     </div>
   );
